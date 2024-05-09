5073602914c18dc39fc1968f514d09cd312e45ed
jasperalen07 <125176411+jasperalen07@users.noreply.github.com>
2024-05-08 19:41:20 -0600

    starting the javascript

--- to-do-list/script/index.js
+++ to-do-list/script/index.js
@@ -1,21 +1,4 @@
-const listsContainer = document.querySelector('[data.lists]')
-
-let lists = ['name', 'to do'];
-
-function render() {
-    <li class="list-name">Work</li>
-    clearElement(listsContainer)
-    lists.forEach(list => {
-        const listElement = document.createElement('li')
-        listElement.classLists.add('list-name')
-        listElement.innerText = list
-        listsContainer.appendChild(listElement)
-
-    })
-}
-
-function clearElement(element) {
-
-}
-
-render()
+// Variables for the section, template, and button
+const toDoItems = documentGetElementById('todo');
+const itemTemplate = documentGetElementById('itemTemplate');
+const addButton = documentGetElementById('add');
