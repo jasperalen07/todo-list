8fe8ae5cd58dd95c7d8c288cc43be8abf4d6413c
jasperalen07 <125176411+jasperalen07@users.noreply.github.com>
2024-05-08 21:34:12 -0600

    started the first phase of javascript

--- to-do-list/script/index.js
+++ to-do-list/script/index.js
@@ -1,4 +1,16 @@
 // Variables for the section, template, and button
-const toDoItems = documentGetElementById('todo');
-const itemTemplate = documentGetElementById('itemTemplate');
-const addButton = documentGetElementById('add');
+const toDoItems = document.getElementById('todo');
+
+const itemTemplate = document.getElementById('itemTemplate');
+const addButton = document.getElementById('add');
+
+//fetching item using local storage
+
+function getItems() {
+    const value = localStorage.getItem('todo') || "[]";
+
+    console.log(value);
+}
+
+getItems();
+
